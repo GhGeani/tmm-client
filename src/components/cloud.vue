0af1ad1893d1f9c8aadbf7abeb5b64802cd9fce0
Gherghe Geani <gherghe_geani@yahoo.com>
2020-01-26 22:39:09 +0200

    Update landing. Add some clouds.

---
+++ src/components/cloud.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+  .cloud
+    .bubble#thirteenth.bg-english
+    .bubble#first.bg-silver
+    .bubble#second.bg-vanila
+    .bubble#third.bg-silver
+    .bubble#fourth.bg-vanila
+    .bubble#fifth.bg-english
+    .bubble#sixth.bg-vanila
+    .bubble#seventh.bg-silver
+    .bubble#eighth.bg-silver
+    .bubble#ninth.bg-english
+    .bubble#tenth.bg-vanila
+    .bubble#eleventh.bg-silver
+    .bubble#twelfth.bg-silver
+</template>
+
+<style lang="stylus" scoped>
+random(min,max)
+  return math(0, 'random')*(max - min + 1) + min
+
+.cloud
+  position absolute
+  margin 2rem
+  width 14rem
+  height 5rem
+  border-radius 30rem
+  opacity .6
+  transition 10s
+  .bubble
+    position absolute
+    height 10rem
+    width 11rem
+    border-radius 100rem
+  #first
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(187, 173, 160, 1)
+  #second
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(209, 198, 173, 1)
+  #third
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(187, 173, 160, 1)
+  #fourth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(209, 198, 173, 1)
+  #fifth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(161, 134, 158, 1)
+  #sixth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(209, 198, 173, 1)
+  #seventh
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(187, 173, 160, 1)
+  #eighth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(187, 173, 160, 1)
+  #ninth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(161, 134, 158, 1)
+  #tenth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(209, 198, 173, 1)
+  #eleventh
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(187, 173, 160, 1)
+  #twelfth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(187, 173, 160, 1)
+  #thirteenth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(209, 198, 173, 1)
+  #fourteenth
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    box-shadow .5rem .2rem 2rem 1rem rgba(161, 134, 158, 1)
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,14 +1,22 @@
 <template lang="pug">
-  main
-    nav-bar
-    .container
-      letters-list(v-if="status === 200" :letters="data")
-      .empty(v-else)  {{ data }}
+  main.conainer.h-100
+    cloud#cloud-one
+    cloud#cloud-two
+    cloud#cloud-tree
+    cloud#cloud-four
+    .row.h-100.align-items-center.justify-content-center
+      .col-8.col-sm-4.col-lg-3
+        .card.tmm-card.bg-rhythm
+          .card-header
+            img.card-img-top(src="../assets/logo.svg" height="80")
+          .card-body
+            router-link.tmm-btn.text-decoration-none.bg-oxford(to="/c") See Confesions
+          .card-footer
 
 </template>
 
 <script>
-import navBar from '../components/navBar.vue';
+import cloud from '../components/cloud.vue';
 import lettersList from '../components/lettersList.vue';
 
 export default {
@@ -19,8 +27,8 @@ export default {
     };
   },
   components: {
-    navBar,
     lettersList,
+    cloud,
   },
   mounted() {
     this.handleResponse();
@@ -40,5 +48,35 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+.tmm-card
+  opacity .7
+@keyframes move
+  0%
+    margin-left -100rem
+  100%
+    margin-left 100%
+#cloud-one
+  bottom 10rem
+  left -2rem
+  animation move 15s linear infinite
+  transform scale(1.5)
+  opacity .4
+#cloud-two
+  top 10rem
+  left -20rem
+  animation move 25s linear infinite
+  transform scale(2)
+  opacity .3
+#cloud-tree
+  top 30rem
+  left 30rem
+  animation move 20s linear infinite
+  transform scale(2)
+  opacity .3
+#cloud-four
+  top -5rem
+  left 30rem
+  animation move 40s linear infinite
+  transform scale(2)
+  opacity .4
 </style>
