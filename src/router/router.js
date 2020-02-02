c4b302374ed70ce62a732d0719c1ae3b91b7ad56
Gherghe Geani <gherghe_geani@yahoo.com>
2020-02-02 22:35:38 +0200

    Create route for confs. Create view

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Landing from '../views/Landing.vue';
+import Confession from '../views/Confession.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Landing',
       component: Landing,
+    },
+    {
+      path: '/c',
+      name: 'Confessions',
+      component: Confession,
     },
   ],
 });
---
+++ src/views/Confession.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .container-fluid.vh-100.vw-100.bg-dark
+</template>
+
+<script>
+export default {};
+</script>
