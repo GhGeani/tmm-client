24cd19292f9baadeaf59615c19963bc465397d6c
Geani Gherghe <gherghe_geani@yahoo.com>
2020-02-22 16:05:00 +0200

    Keep it simple :)

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,8 +1,6 @@
 <template lang="pug">
   main.conainer.h-100#landing
     .row.h-100.align-items-center.justify-content-center
-      cloud#c1
-      cloud#c2
       .col-8.col-sm-4.col-lg-3
         .card.tmm-card.bg-dark
           .card-header
@@ -14,34 +12,11 @@
 </template>
 
 <script>
-import cloud from '../components/cloud.vue';
 import lettersList from '../components/lettersList.vue';
 
 export default {
   components: {
     lettersList,
-    cloud,
   },
 };
 </script>
-
-<style lang="stylus" scoped>
-.tmm-card
-  opacity .9
-@keyframes move
-  0%
-    margin-left -100rem
-  100%
-    margin-left 100%
-#c1
-  top 5%
-  left 5%
-  animation move 15s linear infinite
-  opacity .3
-  transform scale(2)
-#c2
-  left 1%
-  opacity .3
-  transform scale(2)
-
-</style>
