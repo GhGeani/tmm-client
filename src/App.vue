badb52bba171944b68f460d52527506fcafbb9d8
Geani Gherghe <gherghe_geani@yahoo.com>
2020-02-22 19:26:37 +0200

    Reduce cancer. Simplify.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    nav-bar
     router-view
 </template>
 
@@ -15,55 +16,10 @@ export default {
 </script>
 
 <style lang="stylus">
-
-// COLORS
-$dark-vanila = rgba(209, 198, 173, 1)
-$silver-pink = rgba(187, 173, 160, 1)
-$english-lavender = rgba(161, 134, 158, 1)
-$rhythm = rgba(121, 117, 150, 1)
-$oxford-blue = rgba(11, 29, 81, 1)
-
-.bg-oxford
-  background-color $oxford-blue !important
-.bg-rhythm
-  background-color $rhythm !important
-.bg-silver
-  background-color $silver-pink !important
-.bg-english
-  background-color $english-lavender !important
-.bg-vanila
-  background-color $dark-vanila !important
-
-.text-vanila
-  text-color $dark-vanila !important
-.text-silver
-  text-color $silver-pink !important
-
-// TYPOGRAPHY
-.quote
-  font-family 'Indie Flower', cursive
-  font-size 1.1rem
-  color $dark-vanila
-  font-style italic
-
-// DISPLAY APP ALWAYS FULL SCREEN
-#landing
-  position fixed
-  width  100%
-  height 100%
-  left  0
-  top 0
-  z-index 10
-  background-image radial-gradient(
-    circle,
-    alpha(black, 0.4),
-    alpha(black, 0.9),
-    alpha(black, 1))
 .tmm-btn
   transition all .2s ease-in-out
   display block
   padding 0.5rem
-  color $dark-vanila
   text-align center
   &:hover
     transition 0.2s
@@ -72,5 +28,4 @@ $oxford-blue = rgba(11, 29, 81, 1)
   &:active
     transition 0.2s
     transform scale(.95)
-
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,14 +1,5 @@
 <template lang="pug">
   main.conainer.h-100#landing
-    .row.h-100.align-items-center.justify-content-center
-      .col-8.col-sm-4.col-lg-3
-        .card.tmm-card.bg-dark
-          .card-header
-            img.card-img-top(src="../assets/logo.svg" height="80")
-          .card-body
-            router-link.tmm-btn.text-decoration-none.bg-dark(to="/c") See Confesions
-          .card-footer
-
 </template>
 
 <script>
@@ -20,3 +11,19 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus" scoped>
+// DISPLAY APP ALWAYS FULL SCREEN
+#landing
+  position fixed
+  width  100%
+  height 100%
+  left  0
+  top 0
+  z-index 10
+  background-image radial-gradient(
+    circle,
+    alpha(black, 0),
+    alpha(black, .9),
+    alpha(black, 1))
+</style>
