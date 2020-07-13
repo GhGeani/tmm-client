fbde3536244f872153e4a0b133de798b8405566e
Geani Gherghe <geani.gherghe@caphyon.com>
2020-07-14 00:03:27 +0300

    Final adjust sky+clouds

---
+++ src/components/cloud.vue
@@ -0,0 +1,40 @@
+
+<template lang="pug">
+  .cloud
+    .bubble#bubble-1.bg-english
+    .bubble#bubble-2.bg-silver
+    .bubble#bubble-3.bg-vanila
+</template>
+
+<style lang="stylus" scoped>
+random(min,max)
+  return math(0, 'random')*(max - min + 1) + min
+.cloud
+  position absolute
+  margin 2rem
+  width 14rem
+  height 5rem
+  border-radius 30rem
+  opacity .6
+  transition 10s
+  .bubble
+    position absolute
+    height 7rem
+    width 21rem
+    border-radius 55rem
+  #bubble-1
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    background-color rgba(11, 1, 51, 1)
+    box-shadow 7px -2px 12rem 12rem rgba(11, 1, 51, 1)
+  #bubble-2
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    background-color rgba(11, 29, 81, 1)
+    box-shadow 7px -2px 8rem 8rem rgba(11, 29, 81, 1)
+  #bubble-3
+    margin-left random(-2, 25)rem
+    margin-top random(-4, 2)rem
+    background-color rgba(11, 1, 51, 1)
+    box-shadow 7px -2px 8rem 8rem rgba(11, 1, 51, 1)
+</style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
   main.conainer.h-100#landing
+    cloud#cloud-one
+    cloud#cloud-two
+    cloud#cloud-tree
+    cloud#cloud-four
 </template>
+
+<script>
+import cloud from '../components/cloud.vue';
+
+export default {
+  components: {
+    cloud,
+  },
+};
+</script>
 
 <style lang="stylus" scoped>
 #landing
@@ -12,7 +26,53 @@
   z-index 10
   background-image radial-gradient(
     circle,
-    alpha(black, 0.4),
-    alpha(black, 0.9),
-    alpha(black, 1))
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(209, 198, 173, 1), 1),
+    alpha(rgba(11, 29, 81, 1), .8),
+    alpha(rgba(11, 29, 81, 1), .82),
+    alpha(rgba(11, 29, 81, 1), .85),
+    alpha(rgba(11, 29, 81, 1), .87),
+    alpha(rgba(11, 29, 81, 1), .89),
+    alpha(rgba(11, 24, 81, 1), .9),
+    alpha(rgba(11, 24, 81, 1), .93),
+    alpha(rgba(11, 15, 81, 1), .94),
+    alpha(rgba(11, 15, 81, 1), .95),
+    alpha(rgba(11, 7, 81, 1), .96),
+    alpha(rgba(11, 7, 81, 1), .97),
+    alpha(rgba(11, 0, 81, 1), .98),
+    alpha(rgba(11, 0, 81, 1), .99),
+    alpha(rgba(11, 0, 50, 1), 1))
+
+@keyframes move
+  0%
+    margin-left -100rem
+  100%
+    margin-left 100%
+#cloud-one
+  bottom 10rem
+  left -2rem
+  animation move 15s linear infinite
+  opacity .3
+#cloud-two
+  top 10rem
+  left -20rem
+  animation move 20s linear infinite
+  opacity .4
+#cloud-tree
+  top 30rem
+  left 30rem
+  // animation move 15s linear infinite
+  opacity .2
+#cloud-four
+  top -5rem
+  left 30rem
+  // animation move 10s linear infinite
+  opacity .4
 </style>
